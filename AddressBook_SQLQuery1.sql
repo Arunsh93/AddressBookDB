097c6a7f85596d18bdf55ebe3f5630b4c8aec9ca
Arunsh93 <Arunhosur93@gmail.com>
2021-10-25 13:57:11 +0530

    Create Address Book Table

--- AddressBook_SQLQuery1.sql
+++ AddressBook_SQLQuery1.sql
@@ -1,2 +1,12 @@
 Create database AddressBookService;
 use AddressBookService;
+
+Create table AddressBookTable
+(FirstName varchar(100) Not null, 
+ LastName varchar(100) Not null,
+ Address varchar(100) not null,
+ City varchar(50) null,
+ State varchar(50) null,
+ ZipCode int null,
+ PhoneNumber int null,
+ EmailId varchar(50) null);
